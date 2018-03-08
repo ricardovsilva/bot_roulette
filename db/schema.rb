0832153fb689e82938ed2a0f8debfee39cd9f745
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2018-03-08 13:12:27 -0300

    Closes #3. Makes every entry belongs to an user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307234802) do
+ActiveRecord::Schema.define(version: 20180308152510) do
 
   create_table "entries", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180307234802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "active"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
